4a45a628f8e15e28c5e8a66a18890424f8a6cff2
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-13 12:17:26 +0300

    Rotate a 2D matrix in place

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,8 +4,7 @@
 
 def rotate_2d_matrix(matrix):
     """Rotate the  matrix in place"""
-    n = len(matrix
-    )
+    n = len(matrix)
     for i in range(n // 2):
         for j in range(i, n - i - 1):
             matrix[i][j], matrix[~j][i], matrix[~i][~j], matrix[j][~i] = \
